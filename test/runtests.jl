11383da40089c9e3672175b672b6d7f11020dc24
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 09:59:16 +0200

    Use Compat.Test instead of Base.Test for Julia v0.7 compatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of GERDAMetadata.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package GERDAMetadata" begin
+import Compat.Test
+Test.@testset "Package GERDAMetadata" begin
     include.([
     ])
 end
