62ab7c77f211f793b1af67ec2cff0568a416eafa
Florian Henkes <florian.henkes@outlook.com>
2024-02-05 15:25:33 +0100

    New tests for unit and uncertatinty reading

---
+++ test/test_units.jl
@@ -0,0 +1,53 @@
+# This file is a part of jl, licensed under the MIT License (MIT).
+
+using LegendDataManagement
+using Test
+
+using Dates
+using PropDicts
+using JSON
+using Unitful, Measurements
+
+using LegendDataManagement: PropsDB, AnyProps, ValiditySelection
+
+include("testing_utils.jl")
+
+@testset "units" begin
+    a = """
+{
+"A00000": {
+        "n": 500,
+        "a": {
+            "val": 1.55,
+            "err": 0.01,
+            "unit": "μs"
+        },
+        "b": {
+            "val": 15.5,
+            "unit": "μs"
+        }, 
+        "c": {
+            "val": 155,
+            "err": 1
+        }
+    }
+}
+"""
+    pd = PropDict(JSON.parse(a))
+
+    @test pd isa PropDict
+
+    pd = uparse(pd)
+    @test pd.A00000.a.val isa Unitful.Quantity
+    @test pd.A00000.a.err isa Unitful.Quantity
+    @test pd.A00000.b isa Unitful.Quantity
+
+    pd = measurement(pd)
+    @test pd.A00000.a isa Unitful.Quantity
+    @test ustrip(pd.A00000.a) isa Measurements.Measurement
+    @test pd.A00000.b isa Unitful.Quantity
+    @test pd.A00000.c isa Measurements.Measurement
+
+    @test PropDict(JSON.parse(a)) == mstrip(ustrip(measurement(uparse(PropDict(JSON.parse(a))))))
+
+end
