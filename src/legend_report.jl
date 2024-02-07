67ed6ab54282f8fefca697073207d9f08f04ecfa
Oliver Schulz <oschulz@mpp.mpg.de>
2024-02-07 18:49:48 +0100

    Change LegendReport API, add lreport

--- src/legend_report.jl
+++ src/legend_report.jl
@@ -1,38 +1,88 @@
 # This file is a part of LegendDataManagement.jl, licensed under the MIT License (MIT).
 
 
-_table_columnnames(tbl) = keys(Tables.columns(tbl))
-_default_table_headermap(tbl) = Dict(k => string(k) for k in _table_columnnames(tbl))
+"""
+    struct LegendReport
 
-_markdown_cell_content(@nospecialize(x)) = x
-_markdown_cell_content(@nospecialize(x::AbstractString)) = String(x)
-_markdown_cell_content(@nospecialize(x::Symbol)) = string(x)
-_markdown_cell_content(@nospecialize(x::Expr)) = string(x)
-_markdown_cell_content(@nospecialize(x::Number)) = _show_plain_compact(x)
-_markdown_cell_content(@nospecialize(x::AbstractInterval)) = _show_plain_compact(x)
-_markdown_cell_content(@nospecialize(x::Array)) = _show_plain_compact(x)
+Represents a LEGEND report.
 
-_show_plain_compact(x) = sprint(show, x; context = :compact=>true)
+Don't instantiate directly, use [`lreport()`](@ref)
+"""
+struct LegendReport
+    _contents::AbstractVector
+end
 
 
-function _markdown_table(
-    tbl;
-    headermap::Dict{Symbol,<:AbstractString} = _default_table_headermap(tbl),
-    align::AbstractVector{Symbol} = fill(:l, length(Tables.columns(tbl)))
-)
-    content = Vector{Any}[Any[headermap[k] for k in keys(Tables.columns(tbl))]]
-    for r in Tables.rows(tbl)
-        push!(content, [_markdown_cell_content(x) for x in values(r)])
+LegendReport() = LegendReport(Any[])
+
+function Base.show(@nospecialize(io::IO), rpt::LegendReport)
+    for x in rpt._contents
+        show(io, x)
+        println(io)
     end
-    Markdown.Table(content, align)
+end
+
+
+function Base.show(@nospecialize(io::IO), mime::MIME"text/plain", rpt::LegendReport)
+    r_conv = lreport_for_show!(lreport(), mime, rpt)
+    for x in r_conv._contents
+        _show_report_element_plain(io, x)
+        println(io)
+    end
+end
+
+function _show_report_element_plain(@nospecialize(io::IO), @nospecialize(x))
+    show(io, MIME("text/plain"), x)
+end
+
+
+function Base.show(@nospecialize(io::IO), mime::MIME"text/html", rpt::LegendReport)
+    r_conv = lreport_for_show!(lreport(), mime, rpt)
+    for x in r_conv._contents
+        _show_report_element_html(io, x)
+        println(io)
+    end
+end
+
+function _show_report_element_html(@nospecialize(io::IO), @nospecialize(x))
+    if showable(MIME("text/html"), x)
+        show(io, MIME("text/html"), x)
+    else
+        _show_report_element_plain(io, x)
+    end
+end
+
+
+function Base.show(@nospecialize(io::IO), mime::MIME"text/markdown", rpt::LegendReport)
+    r_conv = lreport_for_show!(lreport(), mime, rpt)
+    for x in r_conv._contents
+        _show_report_element_markdown(io, x)
+        #println(io)
+    end
+end
+
+function _show_report_element_markdown(@nospecialize(io::IO), @nospecialize(x))
+    if showable(MIME("text/markdown"), x)
+        show(io, MIME("text/markdown"), x)
+    else
+        _show_report_element_html(io, x)
+    end
+end
+
+
+function Base.show(@nospecialize(io::IO), ::MIME"juliavscode/html", rpt::LegendReport)
+    show(io, MIME("text/html"), rpt)
 end
 
 
 
 """
-    struct LegendReport
+    lreport()
+    lreport(contents...)
 
-Represents a LEGEND report, e.g. a data processing report.
+Generate a LEGEND report, e.g. a data processing report.
+
+Use [`lreport!(rpt, contents...)`](@ref) to add more content to a report.
 
 Example:
 
@@ -46,102 +96,163 @@ tbl = StructArray(
     col7 = [:(a[1]), :(a[2]), :(a[3]), :(a[4]), :(a[5])]
 )
 
-report = LegendReport()
+rpt = lreport(
+    "# New report",
+    "Table 1:", tbl
+)
+lreport!(rpt, "Figure 1:", stephist(randn(10^3)))
+lreport!(rpt, "Figure 2:", histogram2d(randn(10^4), randn(10^4), format = :png))
 
-push!(report, "# New report")
-push!(report, "Table 1:")
-push!(report, tbl)
-push!(report, "Figure 1:")
-push!(report, stephist(randn(1000)))
+show(stdout, MIME"text/plain"(), rpt)
+show(stdout, MIME"text/html"(), rpt)
+show(stdout, MIME"text/markdown"(), rpt)
 
-show(stdout, MIME"text/plain"(), report)
-show(stdout, MIME"text/html"(), report)
-show(stdout, MIME"text/markdown"(), report)
+writelreport("report.txt", rpt)
+writelreport("report.html", rpt)
+writelreport("report.md", rpt)
 ```
+
+See [`LegendDataManagement.lreport_for_show!`](@ref) for how to specialize the
+behavior of `show` for specific report content types.
 """
-struct LegendReport
-    _elements::AbstractVector
-end
-export LegendReport
+function lreport end
+export lreport
 
-LegendReport() = LegendReport(Any[])
 
-function Base.show(io::IO, r::LegendReport)
-    for x in r._elements
-        show(io, x)
-        println(io)
+lreport() = LegendReport(Any[])
+function lreport(contents...)
+    rpt = lreport()
+    for content in contents
+        lreport!(rpt, content)
     end
+    return rpt
 end
 
 
-function Base.show(io::IO, ::MIME"text/plain", r::LegendReport)
-    for x in r._elements
-        _show_report_element_plain(io, x)
-        println(io)
+"""
+    lreport!(rpt::LegendReport, contents...)
+
+Add more content to report `rpt`. See [`lreport`](@ref) for an example.
+"""
+function lreport! end
+export lreport!
+
+
+function lreport!(rpt::LegendReport, @nospecialize(content))
+    push!(rpt._contents, content)
+    return rpt
+end
+
+function lreport!(rpt::LegendReport, @nospecialize(contents...))
+    for content in contents
+        lreport!(rpt, content)
     end
-end
-
-function _show_report_element_plain(io::IO, x)
-    show(io, MIME("text/plain"), x)
+    return rpt
 end
 
 
-function Base.show(io::IO, ::MIME"text/html", r::LegendReport)
-    for x in r._elements
-        _show_report_element_html(io, x)
-        println(io)
-    end
-end
+function lreport!(rpt::LegendReport, content::Markdown.MD)
+    # Need to make a copy here to prevent recursive self-modification during
+    # show-transformation:
+    content_content_copy = copy(content.content)
 
-function _show_report_element_html(io::IO, x)
-    if showable(MIME("text/html"), x)
-        show(io, MIME("text/html"), x)
+    if isempty(rpt._contents) || !(rpt._contents[end] isa Markdown.MD)
+        push!(rpt._contents, Markdown.MD(content_content_copy))
     else
-        _show_report_element_plain(io, x)
+        append!(rpt._contents[end].content, content_content_copy)
     end
+    return rpt
+end
+
+function lreport!(rpt::LegendReport, @nospecialize(markdown_str::AbstractString))
+    lreport!(rpt, Markdown.parse(markdown_str))
 end
 
 
-function Base.show(io::IO, ::MIME"text/markdown", r::LegendReport)
-    for x in r._elements
-        _show_report_element_markdown(io, x)
-        #println(io)
+"""
+    LegendDataManagement.lreport_for_show!(rpt::LegendReport, mime::MIME, content)
+
+Add the contents of `content` to `rpt` in a way that is optimized for being
+displayed (e.g. via `show`) with the given `mime` type.
+
+`show(output, mime, rpt)` first transforms `rpt` by converting all contents of
+`rpt` using `lreport_for_show!(rpt::LegendReport, mime, content)`.
+
+Defaults to `lreport!(rpt, content)`, except for tables
+(`Tables.istable(content) == true`), which are converted to Markdown tables
+by default for uniform appearance.
+
+`lreport_for_show!` is not inteded to be called by users, but to be
+specialized for specific types of content `content`. Content types not already
+supported will primarily require specialization of
+
+```julia
+lreport_for_show!(rpt::LegendReport, ::MIME"text/markdown", content::SomeType)
+```
+
+In some cases it may be desireable to specialize `lreport_for_show!` for
+MIME types like `MIME"text/html"` and `MIME"text/plain"` as well.
+"""
+function lreport_for_show! end
+
+function lreport_for_show!(rpt::LegendReport, mime::MIME, content::LegendReport)
+    for c in content._contents
+        lreport_for_show!(rpt, mime, c)
     end
+    return rpt
 end
 
-function _show_report_element_markdown(io::IO, x)
-    if showable(MIME("text/markdown"), x)
-        show(io, MIME("text/markdown"), x)
+function lreport_for_show!(rpt::LegendReport, ::MIME, @nospecialize(content))
+    if Tables.istable(content)
+        lreport!(rpt, Markdown.MD(_markdown_table(content)))
     else
-        _show_report_element_html(io, x)
+        lreport!(rpt, content)
     end
 end
 
+_table_columnnames(tbl) = keys(Tables.columns(tbl))
+_default_table_headermap(tbl) = Dict(k => string(k) for k in _table_columnnames(tbl))
 
-Base.show(io::IO, ::MIME"juliavscode/html", r::LegendReport) = show(io, MIME("text/html"), r)
+_markdown_cell_content(@nospecialize(content)) = content
+_markdown_cell_content(@nospecialize(content::AbstractString)) = String(content)
+_markdown_cell_content(@nospecialize(content::Symbol)) = string(content)
+_markdown_cell_content(@nospecialize(content::Expr)) = string(content)
+_markdown_cell_content(@nospecialize(content::Number)) = _show_plain_compact(content)
+_markdown_cell_content(@nospecialize(content::AbstractInterval)) = _show_plain_compact(content)
+_markdown_cell_content(@nospecialize(content::Array)) = _show_plain_compact(content)
 
+_show_plain_compact(@nospecialize(content)) = sprint(show, content; context = :compact=>true)
 
-Base.push!(report::LegendReport, @nospecialize(content)) = _report_push!(report, content)
-
-
-function _report_push!(report::LegendReport, @nospecialize(content))
-    push!(report._elements, content)
-    return report
-end
-
-function _report_push!(report::LegendReport, content::Markdown.MD)
-    if isempty(report._elements) || !(report._elements[end] isa Markdown.MD)
-        push!(report._elements, content)
-    else
-        append!(report._elements[end].content, content.content)
+function _markdown_table(
+    tbl;
+    headermap::Dict{Symbol,<:AbstractString} = _default_table_headermap(tbl),
+    align::AbstractVector{Symbol} = fill(:l, length(Tables.columns(tbl)))
+)
+    content = Vector{Any}[Any[headermap[k] for k in keys(Tables.columns(tbl))]]
+    for rpt in Tables.rows(tbl)
+        push!(content, [_markdown_cell_content(content) for content in values(rpt)])
     end
-    return report
+    Markdown.Table(content, align)
 end
 
-function _report_push!(report::LegendReport, @nospecialize(markdown_str::AbstractString))
-    _report_push!(report, Markdown.parse(markdown_str))
+
+"""
+    writelreport(filename::AbstractString, rpt::LegendReport)
+    writelreport(filename::AbstractString, mime::MIME, rpt::LegendReport)
+
+Write lreport `rpt` to file `filename`.
+"""
+function writelreport end
+export writelreport
+
+function writelreport(@nospecialize(filename::AbstractString), @nospecialize(mime::MIME), rpt::LegendReport)
+    open(filename, "w") do io
+        show(io, mime, rpt)
+    end
 end
 
-function _report_push!(report::LegendReport, @nospecialize(tbl::AbstractVector{<:NamedTuple}))
-    _report_push!(report, Markdown.MD(_markdown_table(tbl)))
+function writelreport(@nospecialize(filename::AbstractString), rpt::LegendReport)
+    _, ext = splitext(filename)
+    mime = mime_from_extension(ext)
+    writelreport(filename, mime, rpt)
 end
