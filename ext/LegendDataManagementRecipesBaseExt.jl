94b6d6ee3c91ed8f124119a3a78384cace35c149
Florian Henkes <florian.henkes@outlook.com>
2024-03-19 23:14:52 +0100

    Include recipes base extension

---
+++ ext/LegendDataManagementRecipesBaseExt.jl
@@ -0,0 +1,10 @@
+# This file is a part of LegendSpecFits.jl, licensed under the MIT License (MIT).
+
+module LegendDataManagementRecipesBaseExt
+
+using RecipesBase
+using Unitful, Measurements
+using Measurements: value, uncertainty
+
+
+end # module LegendDataManagementRecipesBaseExt
