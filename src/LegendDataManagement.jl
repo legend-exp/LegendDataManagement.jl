bb5e990dd336dde475dee77a2204421902d4840e
Florian Henkes <florian.henkes@outlook.com>
2024-08-08 16:31:20 +0200

    Remove Requires support

--- src/LegendDataManagement.jl
+++ src/LegendDataManagement.jl
@@ -33,8 +33,6 @@ using TypedTables
 import Markdown
 using MIMEs: mime_from_extension
 
-import ThreadPinning
-
 include("legend_report.jl")
 include("status_types.jl")
 include("atomic_fcreate.jl")
@@ -51,17 +49,7 @@ include("dataprod_config.jl")
 include("calibration_functions.jl")
 include("evt_functions.jl")
 include("lprops.jl")
+include("data_io.jl")
 include("utils/utils.jl")
 
-@static if !isdefined(Base, :get_extension)
-    using Requires
-end
-
-function __init__()
-    @static if !isdefined(Base, :get_extension)
-        @require LegendHDF5IO = "c9265ca6-b027-5446-b1a4-febfa8dd10b0" begin
-            include("../ext/LegendDataManagementLegendHDF5IOExt.jl")
-        end        
-    end
-end
 end # module
