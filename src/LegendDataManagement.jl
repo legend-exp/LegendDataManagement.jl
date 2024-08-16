b64e871750abc2f1eae432ae62571d83fe91d0fc
Florian Henkes <florian.henkes@outlook.com>
2024-08-16 16:14:41 +0200

    Removed calfuncs.jl --> LegendSpecFIts.jl: only ljl_propfuncs

--- src/LegendDataManagement.jl
+++ src/LegendDataManagement.jl
@@ -36,7 +36,6 @@ using MIMEs: mime_from_extension
 include("legend_report.jl")
 include("status_types.jl")
 include("atomic_fcreate.jl")
-include("calfunc.jl")
 include("filekey.jl")
 include("dataset.jl")
 include("data_config.jl")
