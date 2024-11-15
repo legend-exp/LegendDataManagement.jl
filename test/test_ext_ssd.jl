b8d54337ba8f42834541006262015035b870214e
Felix Hagemann <hagemann@mpp.mpg.de>
2024-11-15 02:15:42 +0100

    Add tests for reading impurity density values from crystal metadata

--- test/test_ext_ssd.jl
+++ test/test_ext_ssd.jl
@@ -19,8 +19,12 @@ include("testing_utils.jl")
             sim = Simulation{Float64}(l200, detname, crystal_impurity = true)
             @test sim isa Simulation
 
-            # Compare active volume from SSD to active volume from LegendDataManagement
             SolidStateDetectors.apply_initial_state!(sim, ElectricPotential, Grid(sim, max_tick_distance = 0.1u"mm"))
+
+            # Check that all crystals are p-type
+            @test all(sim.q_eff_imp.data .<= 0)
+
+            # Compare active volume from SSD to active volume from LegendDataManagement
             active_volume_ssd = SolidStateDetectors.get_active_volume(sim.point_types)
             active_volume_ldm = LegendDataManagement.get_active_volume(l200.metadata.hardware.detectors.germanium.diodes[Symbol(detname)], 0.0)
             @test isapprox(active_volume_ssd, active_volume_ldm, rtol = 0.01)
